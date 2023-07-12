d741bbd80b1471f6ac45f1c5245a9cee999096cc
Bart <kmiecikjob@gmail.com>
2023-07-12 17:45:43 +0200

    First step, adding header to pass requirments for scrapping data from amazon

--- AmazonProductPriceTracking/main.py
+++ AmazonProductPriceTracking/main.py
@@ -1,2 +1,9 @@
 import requests
 
+headers = {
+    'Accept-Language':'pl-PL,pl;q=0.5',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+}
+request = requests.get('https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6', headers=headers)
+
+print(request.text)
