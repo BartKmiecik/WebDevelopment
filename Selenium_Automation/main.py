095dea126a74a0392a8856dc8c99cf52cfe8ff12
Bart <kmiecikjob@gmail.com>
2023-07-14 19:00:22 +0200

    Welll I struggled thinking selenium doesn't work, looking for information, testing stuff, etc... Turns out python shooting it down after application finish

--- Selenium_Automation/main.py
+++ Selenium_Automation/main.py
@@ -1,8 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_plugin = 'Selenium_Automation/chromedriver.exe'
+chrome_plugin = 'C:/Users/user/Projects/Udemy_Python/WebDevelopments/WebDevelopment/Selenium_Automation/chromedriver.exe'
 
-driver = webdriver.Chrome(chrome_plugin)
 
-driver.get('http://www.google.com/')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
 
+
+while(True):
+    pass
