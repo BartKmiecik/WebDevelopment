c5c54270f53153a0b50770eb55e08594bf8cb9ed
Bart <kmiecikjob@gmail.com>
2023-07-14 20:04:04 +0200

    Progress with selenium, but quite difficult to play with

--- Selenium_Automation/main.py
+++ Selenium_Automation/main.py
@@ -1,13 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 chrome_plugin = 'C:/Users/user/Projects/Udemy_Python/WebDevelopments/WebDevelopment/Selenium_Automation/chromedriver.exe'
 
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.google.com")
+driver.get("https://www.python.org/")
 
+
+event_times = driver.find_elements(By.CSS_SELECTOR, '.event-widget time')
+print(event_times.pop().text)
 
 while(True):
     pass
