c621e5c82d902f4f6cf77b7c35052bba391d5e2d
Bart <kmiecikjob@gmail.com>
2023-07-14 18:49:50 +0200

    Importing chrome drivers but struggling to do implement them

--- Selenium_Automation/main.py
+++ Selenium_Automation/main.py
@@ -1,3 +1,8 @@
-chrome_plugin = 'C:\Users\user\Projects\Python\Chrome_Plugin\chromedriver.exe'
+from selenium import webdriver
 
- 
+chrome_plugin = 'Selenium_Automation/chromedriver.exe'
+
+driver = webdriver.Chrome(chrome_plugin)
+
+driver.get('http://www.google.com/')
+
