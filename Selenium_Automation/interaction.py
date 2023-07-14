c64f53dcefa942ee18791b662dc99de4ed00cd45
Bart <kmiecikjob@gmail.com>
2023-07-14 21:26:05 +0200

    Creating script to autofill user, pass, email and register to newsletter

--- Selenium_Automation/interaction.py
+++ Selenium_Automation/interaction.py
@@ -3,12 +3,22 @@ from selenium.webdriver.common.by import By
 
 webdriver = webdriver.Chrome()
 
-webdriver.get('https://en.wikipedia.org/wiki/Main_Page')
+webdriver.get('http://secure-retreat-92358.herokuapp.com/')
+#
+# header_section = webdriver.find_element(By.CSS_SELECTOR, '.mp-box')
+# whole_line = header_section.find_element(By.ID, 'articlecount')
+# number = whole_line.find_element(By.CSS_SELECTOR, 'a')
+# number.click()
+# print(int(number.text.replace(',', '')))
+name = webdriver.find_element(By.NAME, 'fName')
+name.send_keys('Bart')
+sur = webdriver.find_element(By.NAME, 'lName')
+sur.send_keys('Kmie')
+email = webdriver.find_element(By.NAME, 'email')
+email.send_keys('email@gmail.com')
+btn = webdriver.find_element(By.CLASS_NAME, 'btn')
+btn.click()
 
-header_section = webdriver.find_element(By.CSS_SELECTOR, '.mp-box')
-whole_line = header_section.find_element(By.ID, 'articlecount')
-number = whole_line.find_element(By.CSS_SELECTOR, 'a')
-print(int(number.text.replace(',', '')))
 
 while True:
     pass
