a42ac8053c9f70f74e91e53d8beb02cd59a98191
Bart <kmiecikjob@gmail.com>
2023-07-15 11:43:42 +0200

    Started working on tinder swapper, but facing same issue. Everyingle browser blocking access to google auth and can't get int

--- Linkdein_automation_cv/main.py
+++ Linkdein_automation_cv/main.py
@@ -1,9 +1,9 @@
 # selenium 4
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 #service = Service('WebDevelopments/WebDevelopment/chromedriver.exe')
 #driver = webdriver.Chrome(service=service)
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
---
+++ TinderSwapper/main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+
+webdriver = webdriver.Chrome()
+webdriver.get('https://tinder.com/')
+
+while True:
+    pass
