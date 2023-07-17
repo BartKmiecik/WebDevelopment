da8057898bde588d25397674fbbe39bacab03327
Bart <kmiecikjob@gmail.com>
2023-07-17 19:28:59 +0200

    Playing with app routs and variables

--- Falsk_beggining/hello.py
+++ Falsk_beggining/hello.py
@@ -1,25 +1,36 @@
-# from flask import Flask
-# app = Flask(__name__)
+# import time
 #
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+# def time_decoratr(fun):
+#     start = time.time()
+#     fun()
+#     end = time.time()
+#     running_time = end - start
+#     print(running_time)
 #
-# if __name__ == "__main__":
-#     app.run()
+# @time_decoratr
+# def do_smt():
+#     time.sleep(1)
+#
+# while True:
+#     pass
 
-import time
+from flask import Flask, url_for
+from markupsafe import escape
 
-def time_decoratr(fun):
-    start = time.time()
-    fun()
-    end = time.time()
-    running_time = end - start
-    print(running_time)
+app = Flask(__name__)
 
-@time_decoratr
-def do_smt():
-    time.sleep(1)
+@app.route('/')
+def home_index():
+    return 'Home page'
 
-while True:
-    pass
+@app.route('/hello')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/username/<user>')
+def greet_user(user):
+    return f'Hello {user}'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
