f8b58853e89eeb93578de48f62f6e18b47b7988d
Bart <kmiecikjob@gmail.com>
2023-07-17 20:11:29 +0200

    Sending and wrapping functions

--- Falsk_beggining/hello.py
+++ Falsk_beggining/hello.py
@@ -14,6 +14,28 @@
 # while True:
 #     pass
 
+
+from functools import wraps
+def make_bold(fun):
+    @wraps(fun)
+    def decorated_function(*args, **kwargs):
+        smt = fun(*args, **kwargs)
+        return f'<b>{smt}</b>'
+    return decorated_function
+def make_en(fun):
+    @wraps(fun)
+    def decorated_function(*args, **kwargs):
+        smt = fun(*args, **kwargs)
+        return f'<en>{smt}</en>'
+    return decorated_function
+
+def make_u(fun):
+    @wraps(fun)
+    def decorate_function(*args, **kwargs):
+        additional_text = f"{args}" + 'bd'
+        return fun(additional_text)
+    return decorate_function
+
 from flask import Flask, url_for
 from markupsafe import escape
 
@@ -23,11 +45,14 @@ app = Flask(__name__)
 def home_index():
     return 'Home page'
 
-@app.route('/hello')
+@app.route('/hello/')
+@make_bold
+@make_en
 def hello_world():
     return 'Hello, World!'
 
 @app.route('/username/<user>')
+@make_u
 def greet_user(user):
     return f'Hello {user}'
 
