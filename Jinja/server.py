a2e6d96744508389bec112ff5c8388058ce0e850
Bart <kmiecikjob@gmail.com>
2023-07-20 18:52:16 +0200

    Creating index html, working on guess page

--- Jinja/server.py
+++ Jinja/server.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+import datetime as dt
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    rand = random.randint(0,10)
+    year = dt.datetime.now()
+    return render_template('index.html', rand=rand, year=(year.strftime('%Y')))
+
+@app.route('/guess/{username}')
+def hello_world():
+    return render_template('index.html', rand=rand, year=(year.strftime('%Y')))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
