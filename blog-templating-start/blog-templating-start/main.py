8c6c8bfecba72befd3f8314c98a8db5f3fbab560
Bart <kmiecikjob@gmail.com>
2023-07-20 21:17:41 +0200

    Fake blog posts with same template, but passing different values

--- blog-templating-start/blog-templating-start/main.py
+++ blog-templating-start/blog-templating-start/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+import requests
 
 app = Flask(__name__)
 
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/post/<post>')
+def get_post(post):
+    request = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+    print(request)
+    return render_template("post.html", post=request.json()[int(post)])
+
 if __name__ == "__main__":
+    request = requests.get('https://api.npoint.io/c790b4d5cab58020d391')
+    print(request.json())
     app.run(debug=True)
