b4a696d75ae75e51e5ad96fb05cd88586ef3f5fc
Bart <kmiecikjob@gmail.com>
2023-07-20 19:16:48 +0200

    Building guessing page that sending & recieve request to 2 apis

--- Jinja/server.py
+++ Jinja/server.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
 import random
 import datetime as dt
+import requests
 
 app = Flask(__name__)
 
+def get_age(name:str):
+    request = requests.get(f'https://api.agify.io?name={name}')
+    return request.text.split(',')[2][6:-1]
+
+
+def get_sex(name:str):
+    request = requests.get(f'https://api.genderize.io?name={name}')
+    return request.text.split(',')[2][10:-1]
 
 @app.route('/')
 def hello_world():
@@ -11,9 +20,13 @@ def hello_world():
     year = dt.datetime.now()
     return render_template('index.html', rand=rand, year=(year.strftime('%Y')))
 
-@app.route('/guess/{username}')
-def hello_world():
-    return render_template('index.html', rand=rand, year=(year.strftime('%Y')))
+
+@app.route('/guess/<username>')
+def guess_human(username):
+    name = username
+    old = get_age(username)
+    sex = get_sex(username)
+    return render_template('guess.html', name=name, sex=sex, old=old)
 
 
 if __name__ == '__main__':
