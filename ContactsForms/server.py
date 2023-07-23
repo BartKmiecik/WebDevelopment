49e69c9dba05aa8b1f71759f406b472743f40959
Bart <kmiecikjob@gmail.com>
2023-07-23 15:16:39 +0200

    Grabbing and ussing fields values from forma html

--- ContactsForms/server.py
+++ ContactsForms/server.py
@@ -1,14 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-@app.route('/login/<user>')
-def login_page(user):
-    return render_template('login_page.html', user=user)
+@app.route('/login', methods=["POST"])
+def login_page():
+    email = request.form['email']
+    password = request.form['password']
+    return render_template('login_page.html', email=email, password=password)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
