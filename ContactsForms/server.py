d29de85ef6e5db89fdd64b58161c66d4690580ce
Bart <kmiecikjob@gmail.com>
2023-07-23 14:47:23 +0200

    Refacotring, adding result of login page

--- ContactsForms/server.py
+++ ContactsForms/server.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('login_page.html')
+    return render_template('index.html')
+
+@app.route('/login/<user>')
+def login_page(user):
+    return render_template('login_page.html', user=user)
 
 
 if __name__ == '__main__':
