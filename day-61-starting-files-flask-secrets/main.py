5dc2d84e8de2b30965cec02e76779a9591cb47f2
Bart <kmiecikjob@gmail.com>
2023-07-24 18:10:27 +0200

    Using wtflask to create login fields

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+import os
+SECRET_KEY = os.urandom(32)
+
+
+class MyForm(FlaskForm):
+    name = StringField('name')
+
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = SECRET_KEY
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    form = MyForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
