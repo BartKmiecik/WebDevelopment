a102dd87eb8f54c064b2c5a485a30d4c8f016d1b
Bart <kmiecikjob@gmail.com>
2023-07-24 18:53:28 +0200

    Fuck yeah, the best Rick Roll I've ever seen

--- day-61-starting-files-flask-secrets/main.py
+++ day-61-starting-files-flask-secrets/main.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import wtforms.validators as validators
 import os
 SECRET_KEY = os.urandom(32)
 
 
 class MyForm(FlaskForm):
-    email = StringField('email')
-    password = PasswordField('password')
+    email = StringField('email', validators=[DataRequired(), validators.Length(min=6, max=120), validators.Email()])
+    password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField("Login")
 
 
@@ -26,6 +27,14 @@ def login():
     if form.validate_on_submit():
         return redirect('/success')
     return render_template('login.html', form=form)
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/denied')
+def denied():
+    return render_template('denied.html')
 
 
 if __name__ == '__main__':
