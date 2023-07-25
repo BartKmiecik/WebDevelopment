07f53fdd41fb7ba96236a46d7452965014c6a000
Bart <kmiecikjob@gmail.com>
2023-07-25 22:21:50 +0200

    Creating flask form

--- day-62-starting-files-coffee-and-wifi/main.py
+++ day-62-starting-files-coffee-and-wifi/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, URL
 import csv
 
 app = Flask(__name__)
@@ -13,6 +13,19 @@ Bootstrap5(app)
 class CafeForm(FlaskForm):
     cafe = StringField('Cafe name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    location = StringField('Location', validators=[DataRequired(),URL])
+    open_time = StringField('Open', validators=[DataRequired()])
+    close_time = StringField('Close', validators=[DataRequired()])
+    coffee_rating = SelectField('Coffe_Rating', choices=[('☕️', '☕️'), ('☕️☕️', '☕️☕️'),
+                                                      ('☕️☕️☕️', '☕️☕️☕️'), ('☕️☕️☕️☕️', '☕️☕️☕️☕️'),
+                                                      ('☕️☕️☕️☕️☕️', '☕️☕️☕️☕️☕️')], validators=[DataRequired()])
+    wifi_rating = SelectField('Wifi_Rating', choices=[('💪', '💪'), ('💪💪', '💪💪'),
+                                                       ('💪💪💪', '💪💪💪'), ('💪💪💪💪', '💪💪💪💪'),
+                                                       ('💪💪💪💪💪', '💪💪💪💪💪')], validators=[DataRequired()])
+
+    power_rating = SelectField('Power_Rating', choices=[('🔌', '🔌'), ('🔌🔌', '🔌🔌'),
+                                                      ('🔌🔌🔌', '🔌🔌🔌'), ('🔌🔌🔌🔌', '🔌🔌🔌🔌'),
+                                                      ('🔌🔌🔌🔌🔌', '🔌🔌🔌🔌🔌')], validators=[DataRequired()])
 
 # Exercise:
 # add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
@@ -57,25 +70,17 @@ def cafes():
     msg = []
     with open('cafe-data.csv', newline='', encoding="utf8") as csv_file:
         csv_data = csv.reader(csv_file, delimiter=',')
-        #list_of_rows = []
         for row in csv_data:
-            #print(f'AAAA {row[4]} BBBBB {row[3]}')
             temp_msg = Message(row)
             msg.append(temp_msg)
-    # msg = Message(list_of_rows)
     data = []
-    #print(msg)
     for m in msg:
-        # print('Test')
-        # print(f"'Cafe Name': {m.cafe_name}, 'Location': {m.location}, 'Open': {m.open}, "
-        #       f"'Close': {m.close}, 'Coffe': {m.coffee}, 'Wifi': {m.wifi}, 'Power': {m.power}")
         if m != msg[0]:
             data.append({'Cafe Name': m.cafe_name, 'Location': m.location, 'Open': m.open,
                      'Close': m.close, 'Coffe': m.coffee, 'Wifi': m.wifi, 'Power': m.power})
     titles = [('Cafe Name', 'Cafe Name'), ('Location', 'Location'), ('Open', 'Open'), ('Close', 'Close'),
               ('Coffe', 'Coffe'), ('Wifi', 'Wifi'), ('Power', 'Power')]
 
-    #print(data)
     return render_template('cafes.html', cafes=data, titles=titles)
 
 
