980666c4166763c0fd9bf46134af5e6e8e171448
Bart <kmiecikjob@gmail.com>
2023-07-26 20:11:41 +0200

    Working on some basic jinjin display, but my mouse is irritating me on win11, constantly double, triple clicks irritating!

--- day-63-starting-files-library-project/main.py
+++ day-63-starting-files-library-project/main.py
@@ -20,17 +20,24 @@ app = Flask(__name__)
 
 all_books = []
 
-def is_range(form, field):
-    if field.data >= 10 or field.data < 0:
-        raise ValidationError('Incorrect integer. Pass number from 0 - 10')
+
+# def is_range(field):
+#     if field.data >= 10 or field.data < 0:
+#         raise ValidationError('Incorrect integer. Pass number from 0 - 10')
+
+
 class Books(Form):
-    title = StringField('title',validators=[DataRequired()])
+    title = StringField('title', validators=[DataRequired()])
     author = StringField('author', validators=[DataRequired()])
-    rating = IntegerField('rating', validators=[DataRequired(),[is_range]])
+    rating = IntegerField('rating', validators=[DataRequired()])
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    all_books.append({'title': 'HarryPotter', 'author': 'J.K.Rowling', 'rating': 9})
+    all_books.append({'title': 'HarryPotter', 'author': 'J.K.Rowling', 'rating': 7})
+    all_books.append({'title': 'HarryPotter', 'author': 'J.K.Rowling', 'rating': 3})
+    print(all_books)
+    return render_template('index.html', library=all_books)
 
 
 @app.route("/add")
