a2bede8c960a632653859437892e9219716cf087
Bart <kmiecikjob@gmail.com>
2023-07-27 18:49:59 +0200

    Creating new venv for flask sql lib

--- SQL_Start/main.py
+++ SQL_Start/main.py
@@ -1,7 +1,20 @@
 import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-db = sqlite3.connect('books-collection.db')
-cursor = db.cursor()
+# db = sqlite3.connect('books-collection.db')
+# cursor = db.cursor()
 
-cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,"
-               " author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,"
+#                " author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+# create the extension
+db = SQLAlchemy()
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+# initialize the app with the extension
+db.init_app(app)
