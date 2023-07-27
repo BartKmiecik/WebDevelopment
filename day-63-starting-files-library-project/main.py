03a4180d8d9e7126df755746bf8764221c1cf851
Bart <kmiecikjob@gmail.com>
2023-07-27 20:05:51 +0200

    Dynamically creating and adding books to db through form

--- day-63-starting-files-library-project/main.py
+++ day-63-starting-files-library-project/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 '''
 Red underlines? Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -15,24 +16,21 @@ This will install the packages from requirements.txt for this project.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key'
 all_books = []
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+db.init_app(app)
 
-
-# def is_range(field):
-#     if field.data >= 10 or field.data < 0:
-#         raise ValidationError('Incorrect integer. Pass number from 0 - 10')
-
-
-# class Books(Form):
-#     title = StringField('title', validators=[DataRequired()])
-#     author = StringField('author', validators=[DataRequired()])
-#     rating = IntegerField('rating', validators=[DataRequired()])
-#     submit = SubmitField()
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer, unique=False, nullable=False)
 
 @app.route('/')
 def home():
-    # all_books.append({'title': 'HarryPotter', 'author': 'J.K.Rowling', 'rating': 9})
-    # all_books.append({'title': 'HarryPotter', 'author': 'J.K.Rowling', 'rating': 7})
-    # all_books.append({'title': 'HarryPotter', 'author': 'J.K.Rowling', 'rating': 3})
+    with app.app_context():
+        db.create_all()
+        all_books = Book.query.order_by(Book.id).all()
     return render_template('index.html', library=all_books)
 
 
@@ -46,8 +44,49 @@ def add():
     #     all_books.append({'title': form.title, 'author': form.author, 'rating': form.rating})
     #
     # len(all_books)
-    return render_template('add.html'   )
+    # print('test')
+    if request.method == 'POST':
+        # book = Book(
+        #     id=1,
+        #     name="Harry Potter",
+        #     author="J. K. Rowling",
+        #     rating=9.
+        # )
+
+        with app.app_context():
+            db.create_all()
+            last_book = Book.query.order_by(Book.id).all()
+            title = request.form['book']
+            author = request.form['author']
+            rating = request.form['rating']
+
+            book = Book(
+                id= last_book[-1].id+1,
+                name=title,
+                author=author,
+                rating=rating
+            )
+            db.session.add(book)
+            db.session.commit()
+
+    return render_template('add.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# import sqlite3
+# from flask import Flask, request
+
+
+# db = sqlite3.connect('books-collection.db')
+# cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE,"
+#                " author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+# db.commit()
+
+# create the extension
+
 
