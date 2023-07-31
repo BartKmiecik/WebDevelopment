a7ebe703075d00644f98c25528e04bfb0bcff5e3
Bart <kmiecikjob@gmail.com>
2023-07-31 19:44:10 +0200

    Returning random name of caffess

--- day-66-starting-files-cafe-api/main.py
+++ day-66-starting-files-cafe-api/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+import random
 '''
 Install the required packages first: 
 Open the Terminal in PyCharm (bottom left). 
@@ -37,13 +37,25 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
 
-with app.create_contect():
-    db.create_all()
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/random")
+def get_random():
+    with app.app_context():
+        db.create_all()
+        query = db.session.execute(db.select(Cafe).order_by(Cafe.id))
+        cafes = query.all()
+        print('AAAAAAAAAAAAAAAAAAAAAA')
+        print(cafes)
+        print(len(cafes))
+        print('BBBBBBBBBBBBBBBBBBB')
+        rand = random.randint(0, len(cafes)-1)
+        print(rand)
+        print(cafes[rand][0].id)
+        return f"<p>{cafes[rand][0].name}</p>"
     
 
 ## HTTP GET - Read Record
