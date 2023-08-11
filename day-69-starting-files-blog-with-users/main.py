82dfe4a0db3c1a8593a7d59bd1c89ce96eff0c9b
Bart <kmiecikjob@gmail.com>
2023-08-11 18:57:22 +0200

    Fixing relationship between tables in database

--- day-69-starting-files-blog-with-users/main.py
+++ day-69-starting-files-blog-with-users/main.py
@@ -54,8 +54,8 @@ class BlogPost(db.Model):
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey("blog_user.id"))
+    author_id = db.relationship("User", back_populates="posts")
+    author = db.Column(db.String(250), db.ForeignKey("blog_user.username"))
     img_url = db.Column(db.String(250), nullable=False)
 
 
@@ -67,7 +67,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(250), unique=True, nullable=False)
     email = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable=False)
-    posts = relationship("BlogPost", back_populates="author_id")
+    posts = db.relationship("BlogPost", back_populates="author_id")
 
 
 with app.app_context():
